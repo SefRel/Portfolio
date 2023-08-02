a4e896fee927b1e9b3281cc7f090a9feea4f39b9
sefrel <akinolaridwanbnhusaain@gmail.com>
2023-08-03 00:48:40 +0100

    portfoiio

---
+++ contact.php
@@ -0,0 +1,39 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+ //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+$mail->Port = 465;
+
+$mail->Username = "ceosefrel@gmail.com";
+$mail->Password = "cnhnmzbkudihjmso";
+
+$mail->setFrom($email, $name);
+$mail->addAddress("ridwan@sefrel.com", "Ridwan");
+
+$mail->Subject = $subject;
+$mail->Body = $message;
+
+$mail->send();
+
+echo "Email sent successfully!"; 
+
+
